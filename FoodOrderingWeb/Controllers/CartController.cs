dc515b03f20bba7821f1f4375753ec8e472a0f0c
nhatminh4403 <145756375+nhatminh4403@users.noreply.github.com>
2024-07-09 17:14:28 +0700

    css js and online template

---
+++ FoodOrderingWeb/Controllers/CartController.cs
@@ -0,0 +1,28 @@
+﻿using FoodOrderingWeb.Repository.Framework;
+using FoodOrderingWeb.Repository.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodOrderingWeb.Controllers
+{
+	public class CartController : Controller
+	{
+
+		private readonly Interface_CartRepository _cartRepository;
+		private readonly Interface_OrderRepository _orderRepository;
+		public CartController(Interface_CartRepository cartRepository, Interface_OrderRepository orderRepository)
+		{
+			_cartRepository = cartRepository;
+			_orderRepository = orderRepository;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Checkout() 
+		{
+			return View();
+		}
+	}
+}
